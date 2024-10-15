186481c9acbfab921e99adfebbba7366e9f98d68
Pizavo <100881045+Pizavo@users.noreply.github.com>
2024-10-16 00:50:04 +0200

    Fixed and optimized all sorting

--- src/main/kotlin/hw3/extensions/List.kt
+++ src/main/kotlin/hw3/extensions/List.kt
@@ -3,13 +3,26 @@ package hw3.extensions
 import hw3.sorting.BubbleSort
 import hw3.sorting.MergeSort
 import hw3.sorting.QuickSort
+import hw3.sorting.Sort
 
-fun <T: Comparable<T>> MutableList<T>.bubbleSort(): Long =
+fun <T: Comparable<T>> List<T>.bubbleSort(): Sort<T> =
 	BubbleSort<T>().sort(this)
 
-fun <T: Comparable<T>> MutableList<T>.quickSort(): Long =
+fun <T: Comparable<T>> List<T>.quickSort(): Sort<T> =
 	QuickSort<T>().sort(this)
 
-fun <T: Comparable<T>> MutableList<T>.mergeSort(): Long =
+fun <T: Comparable<T>> List<T>.mergeSort(): Sort<T> =
 	MergeSort<T>().sort(this)
+
+fun <T: Comparable<T>> List<T>.sortAndPrintComplexity(name: String) {
+	println(name)
+	
+	val bubbleSort = this.bubbleSort()
+	val quickSort = this.quickSort()
+	val mergeSort = this.mergeSort()
+	
+	println("Bubble Sort Complexity: ${bubbleSort.complexity}")
+	println("Quick Sort Complexity: ${quickSort.complexity}")
+	println("Merge Sort Complexity: ${mergeSort.complexity}")
+}
 
