337dba3944b085c40e6dd27d313f47eeedfa0d98
Pizavo <100881045+Pizavo@users.noreply.github.com>
2024-12-07 20:04:00 +0100

    HW8 finished

--- src/main/kotlin/hw8/Main.kt
+++ src/main/kotlin/hw8/Main.kt
@@ -7,17 +7,41 @@ import ram.Tape
 
 
 fun main() {
-	val path1 = resource("ram/1.txt")
-	
-	val tape1 = Tape("1|1|1|2|2|3|3|3")
-	val instructions1 = Instructions.fromFile(path1)
-	
+	val file1 = resource("ram/1.txt")
+	val tape1 = Tape("1|1|1|2|2|2|3|3|3")
+	val instructions1 = Instructions.fromFile(file1)
+
+	println("1. {1ⁿ 2ⁿ 3ⁿ | n ≥ 0}")
+	println("Tape IN: $tape1")
 	RAM(tape1, instructions1).start()
-	
-	/*val file2 = resource("ram/2.txt")
-	
+
+	println()
+
+	val file2 = resource("ram/2.txt")
 	val tape2 = Tape("1|2|3|4|5")
 	val instructions2 = Instructions.fromFile(file2)
-	
-	RAM(tape2, instructions2).start()*/
+
+	println("2. n")
+	println("Tape IN: $tape2")
+	RAM(tape2, instructions2).start()
+
+	println()
+
+	val file3 = resource("ram/3.txt")
+	val tape3 = Tape("0|1|2|3|4|5|6|7|8|9")
+	val instructions3 = Instructions.fromFile(file3)
+
+	println("3. reversed")
+	println("Tape IN: $tape3")
+	RAM(tape3, instructions3).start()
+
+	println()
+
+	val file4 = resource("ram/4.txt")
+	val tape4 = Tape("0|0|1|1|2|2|1|1|0|2|0|1|2|1|0")
+	val instructions4 = Instructions.fromFile(file4)
+
+	println("4. Σ = {0, 1, 2}, remove '1's")
+	println("Tape IN: $tape4")
+	RAM(tape4, instructions4).start()
 }
--- src/main/kotlin/ram/Register.kt
+++ src/main/kotlin/ram/Register.kt
@@ -3,27 +3,23 @@ package ram
 class Register {
 	private var index: Int = 0
 	private val register: MutableList<Int?> = mutableListOf()
-	
+
 	operator fun get(index: Int) =
 		if (index < 1) throw IndexOutOfBoundsException("Register index must be greater than 0")
 		else (index - 1).also { this.index = it }.let { register[it] }
-		
+
 	operator fun set(index: Int, value: Int?) {
 		if (index < 1) throw IndexOutOfBoundsException("Register index must be greater than 0")
 		else (index - 1).also { this.index = it; fill(it) }.let { register[it] = value }
 	}
-	
+
 	private fun fill(index: Int) {
 		while (register.size <= index) register.add(null)
 	}
-	
-	fun offset(offset: Int) =
-		(this.index + offset).let {
-			if (it < 0) throw IndexOutOfBoundsException("Register index must be greater than 0")
-			
-			it
-		}
-	
-	fun offsetted(offset: Int) =
-		register[offset(offset)]
+
+	override fun toString(): String {
+		val filled = register.map { it ?: "#" }
+
+		return filled.joinToString("|", postfix = "|#") { if (it == "#") "#" else it.toString() }
+	}
 }
